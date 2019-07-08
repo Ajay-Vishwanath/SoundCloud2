4344e8f0bd265895a3b171f31cdc775e407255e7
Ajay <ajayvishwanath.94@gmail.com>
2019-07-08 12:22:43 -0700

    Create Comments Controller

---
+++ app/controllers/api/comments_controller.rb
@@ -0,0 +1,33 @@
+class Api::CommentsController < ApplicationController
+
+    def index 
+        @comments = Comment.all 
+    end 
+
+    def create
+        @comment = Comment.new(comment_params)
+        @comment.user_id = current_user.id 
+        if @comment.save 
+            render 'api/comments/show'
+        else 
+            render json @comment.errors.full_messages, status: 401
+        end 
+    end 
+
+    def destroy
+        @comment = Comment.find(params[:id])
+        if @comment 
+            @comment.destroy 
+            render json: params[:id]
+        else 
+            render json: ["This Comment Does Not Exist"], status: 404
+        end 
+    end 
+
+    private
+    def comment_params
+        params.require(:comment).permit(
+            :body, :user_id, :song_id
+            )
+    end 
+end
