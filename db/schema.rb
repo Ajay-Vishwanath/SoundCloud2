f0cd388bc0b21451a88cb8275cc359626e6927fd
Ajay <ajayvishwanath.94@gmail.com>
2020-01-06 17:12:58 -0800

    Bundle Install, Remove Profile Type Column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_29_172757) do
+ActiveRecord::Schema.define(version: 2019_07_08_184546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(version: 2019_10_29_172757) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "profile_type"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
